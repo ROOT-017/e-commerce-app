504856b08efb12ffc8178f02ead20f56cc1d6305
NKWETACHA TERENCE <nkwetachaterence17@gmail.com>
2025-08-11 09:48:43 +0100

    Complete migrate done

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,13 @@
 // vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-
 
 export default defineConfig({
   plugins: [
-    tailwindcss(),
     react({
       jsxRuntime: "classic", // Add this if using old JSX transform
     }),
   ],
-  css: {
-    postcss: "./postcss.config.js", // Explicit path
-  },
   resolve: {
     alias: {
       // Add this if using absolute imports
